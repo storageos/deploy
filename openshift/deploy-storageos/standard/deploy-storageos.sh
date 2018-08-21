cc2ed4806ce234d82bd3f813068f9c19862b6f36
Ferran Castell <ferran.castell@storageos.com>
2018-08-21 10:58:18 +0100

    Don't schedule on Master nodes

--- openshift/deploy-storageos/standard/deploy-storageos.sh
+++ openshift/deploy-storageos/standard/deploy-storageos.sh
@@ -11,7 +11,7 @@ sleep 2
 
 CLUSTER_IP=$(kubectl -n storageos get svc/storageos -o custom-columns=IP:spec.clusterIP --no-headers=true)
 API_ADDRESS=$(echo -n "tcp://$CLUSTER_IP:5705" | base64)
-JOIN=$(kubectl get nodes -o  jsonpath='{ $.items[*].status.addresses[?(@.type=="InternalIP")].address  }' |tr ' ' ',';echo)
+JOIN=$(kubectl get nodes -l node-role.kubernetes.io/compute=true -o  jsonpath='{ $.items[*].status.addresses[?(@.type=="InternalIP")].address  }' |tr ' ' ',';echo)
 
 sed -e "s/<API_ADDRESS>/$API_ADDRESS/" "$manifests/030_interface.yaml_template" >> "$tmp_file"
 echo "---" >> "$tmp_file"
