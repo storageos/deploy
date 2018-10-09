7e1054a27dada65e82e317f8bfe942536853f857
Ferran Castell <ferran.castell@storageos.com>
2018-10-09 16:08:14 +0100

    More usable cli run command

--- k8s/run-cli.sh
+++ k8s/run-cli.sh
@@ -4,5 +4,6 @@ echo "Run the following command to have a shell inside your cluster with Storage
 
 GR='\033[0;32m'
 NC='\033[0m' # No Color
-echo -e "${GR}kubectl -n storageos run --image storageos/cli:1.0.0-rc2 --rm --restart=Never --env STORAGEOS_HOST=\$(kubectl get svc/storageos --namespace storageos -o custom-columns=IP:spec.clusterIP --no-headers=true)  --env STORAGEOS_USERNAME=storageos --env STORAGEOS_PASSWORD=storageos --command -it storageos-cli -- /bin/sh${NC}"
+echo -e "${GR}kubectl -n storageos run --image storageos/cli:1.0.0-rc3 --restart=Never --env STORAGEOS_HOST=\$(kubectl get svc/storageos --namespace storageos -o custom-columns=IP:spec.clusterIP --no-headers=true)  --env STORAGEOS_USERNAME=storageos --env STORAGEOS_PASSWORD=storageos --command cli -- /bin/sleep 999999 ${NC}"
+echo -e "${GR}kubectl -n storageos exec -it cli -- /bin/sh${NC}"
 
