114dcc1c1b87bac2d3b8a14bddd806655dc63eeb
Ferran <ferran.castell@storageos.com>
2019-08-22 08:15:57 +0100

    Remove old installation methods

--- openshift/deploy-storageos/labelled-deployment/cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-oc delete sc/fast
-oc delete -n storageos all --all --now
-oc delete -n storageos sa/storageos role/storageos rolebinding/storageos --now
-oc delete -f ./manifests/
-oc delete ns storageos --now
