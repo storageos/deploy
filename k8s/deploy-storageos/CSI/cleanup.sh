76d39ba66baa57f9cec6baccdc823083a9f5ef50
Ferran Arau <ferran.castell@storageos.com>
2019-08-22 16:17:35 +0100

    Remove old installation methods (#75)

--- k8s/deploy-storageos/CSI/cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl delete sc/fast
-kubectl delete -n storageos all --all --now
-kubectl delete -n storageos sa/storageos role/storageos rolebinding/storageos --now
-kubectl delete -f ./manifests
-kubectl delete ns storageos --now
--- openshift/deploy-storageos/standard/deploy-storageos.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-RE='\033[0;31m'
-NC='\033[0m' # No Color
-echo -e "${RE}You shouldn't be using this repository as it is deprecated, plese check out the README for more information.${NC}"
