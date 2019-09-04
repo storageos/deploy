d6d6464d54fdb358148fdfd00057e9b3f433f325
Stella Englezou <stella.englezou@storageos.com>
2019-09-04 14:17:25 +0100

    Bump up version to 1.4.0

--- k8s/deploy-storageos/cluster-operator/cleanup.sh
+++ k8s/deploy-storageos/cluster-operator/cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl delete -f https://github.com/storageos/cluster-operator/releases/download/1.3.0/storageos-operator.yaml
+kubectl delete -f https://github.com/storageos/cluster-operator/releases/download/1.4.0/storageos-operator.yaml
