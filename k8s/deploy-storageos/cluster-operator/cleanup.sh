d7a6ece7ab00bc747fe3c8bba36c02206b35fe31
avestuk <avestuk@gmail.com>
2020-05-05 18:26:02 +0100

    Version bump to v2.0.0 (#93)
    * Version bump to v2.0.0

--- k8s/deploy-storageos/cluster-operator/cleanup.sh
+++ k8s/deploy-storageos/cluster-operator/cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl delete -f https://github.com/storageos/cluster-operator/releases/download/1.5.3/storageos-operator.yaml
+kubectl delete -f https://github.com/storageos/cluster-operator/releases/download/v2.0.0/storageos-operator.yaml
