91aaf06a09bb6b4db11ec8ddf3042082c2264254
Alex Vest <alex.vest@storageos.com>
2021-03-24 14:49:45 +0000

    version bump to v2.3.3 (#114)

--- k8s/deploy-storageos/cluster-operator/cleanup.sh
+++ k8s/deploy-storageos/cluster-operator/cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl delete -f https://github.com/storageos/cluster-operator/releases/download/v2.3.3/storageos-operator.yaml
+kubectl delete -f https://github.com/storageos/cluster-operator/releases/download/v2.3.4/storageos-operator.yaml
