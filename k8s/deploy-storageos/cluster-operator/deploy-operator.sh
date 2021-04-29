5250b0d8f1ec566311b34cf41672ff82f427c2d6
Alex Vest <alex.vest@storageos.com>
2021-04-29 17:53:55 +0100

    version bump 2.4.0

--- k8s/deploy-storageos/cluster-operator/deploy-operator.sh
+++ k8s/deploy-storageos/cluster-operator/deploy-operator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl create -f https://github.com/storageos/cluster-operator/releases/download/v2.3.4/storageos-operator.yaml
+kubectl create -f https://github.com/storageos/cluster-operator/releases/download/v2.4.0/storageos-operator.yaml
 
 
 GR='\033[0;32m'
